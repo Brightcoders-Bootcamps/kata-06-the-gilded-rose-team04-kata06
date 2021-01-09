09c6d79d2d60876c2248f126dd7d65a1504472be
ferreirarauldev <ferreirarauldev@gmail.com>
2021-01-08 22:22:12 -0300

    Fix bugs on line 16. get type method, the error was that it returned an array not a string. Apply rubocop on rules.

--- app/rules.rb
+++ app/rules.rb
@@ -1,57 +1,61 @@
+# frozen_string_literal: true
+
+# Rules class manage and moduralize the bussiness rules
 class Rules
+  # general class
+  def self.run_rules(item)
+    type = get_type(item)
+    apply_rules(item, type)
+  end
 
-    private 
-    # general class
-    def self.run_rules(item)
-        type = get_type(item)
-        apply_rules(item, type)
-    end
+  private
 
-    # return item type
-    def get_type(item)
-        special_products = ['Backstage', 'Aged Brie', 'Sulfuras', 'Conjured']
-        special_products.select {|product| product.include? item}
-    end
+  # return item type
+  def get_type(item)
+    special_products = ['Backstage', 'Aged Brie', 'Sulfuras', 'Conjured']
+    special_products.select { |product| product.include? item }[0].class
+  end
 
-    def apply_rules(item, type)
-        case type
-        when 'Backstage'
-            backstage_quality_calculator(item)
-        when 'Aged Brie'
-            aged_brie_quality_calculator(item)
-        when 'Sulfuras'
-            sulfuras_quality_calculator(item)
-        when 'Conjured'
-            conjured_quality_calculator(item)
-        else
-            normal_quality_calculator(item)
-        end
+  def apply_rules(item, type)
+    case type
+    when 'Backstage'
+      backstage_quality_calculator(item)
+    when 'Aged Brie'
+      aged_brie_quality_calculator(item)
+    when 'Sulfuras'
+      sulfuras_quality_calculator(item)
+    when 'Conjured'
+      conjured_quality_calculator(item)
+    else
+      normal_quality_calculator(item)
     end
+  end
 
-    # Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
-    def backstage_quality_calculator(item)
-        item.quality += 2 if item.sell_in <= 10
-        item.quality += 3 if item.sell_in <= 5
-        item.quality = 0 if item.sell_in <= 0
-        
-    end
+  # Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days
+  def backstage_quality_calculator(item)
+    item.quality += 2 if item.sell_in <= 10
+    item.quality += 3 if item.sell_in <= 5
+    item.quality = 0 if item.sell_in <= 0
+  end
 
-    # "Aged Brie" actually increases in Quality the older it gets the Quality of an item is never more than 50
-    def aged_brie_quality_calculator(item, type)
-        item.quality += 1 if item.quality < 51
-    end
+  # "Aged Brie" actually increases in Quality the older it gets the Quality of an item is never more than 50
+  def aged_brie_quality_calculator(item, _type)
+    item.quality += 1 if item.quality < 51
+  end
 
-    # Rules for sulfuras (this method defines that Sulfuras cannot be sold and it's quality cannot be less than 80)
-    def sulfuras_quality_calculator(item, type)
-        item.quality = 80
-        item.sell_in = -1
-    end
-    # Rules for conjured these define that the quality of this item degrades twice as much as a normal item  
-    def conjured_quality_calculator(item)
-        item.quality -= 2 if item.quality > 0
-    end
-    # Rules for a normal item these defines that the quality of this item degrades one by one
-    def normal_quality_calculator(item, type)
-        item.quality -= 1 if item.quality > 0
-    end
+  # Rules for sulfuras (this method defines that Sulfuras cannot be sold and it's quality cannot be less than 80)
+  def sulfuras_quality_calculator(item, _type)
+    item.quality = 80
+    item.sell_in = -1
+  end
+
+  # Rules for conjured these define that the quality of this item degrades twice as much as a normal item
+  def conjured_quality_calculator(item)
+    item.quality -= 2 if item.quality.positive?
+  end
+
+  # Rules for a normal item these defines that the quality of this item degrades one by one
+  def normal_quality_calculator(item, _type)
+    item.quality -= 1 if item.quality.positive?
+  end
 end
