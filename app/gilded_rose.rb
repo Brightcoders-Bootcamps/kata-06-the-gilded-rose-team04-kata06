02bbcf64abb63c9616f2fd0f747f1c04f12c6bb3
MiguelOcegueraM <migueloceguera.m@gmail.com>
2021-01-08 18:07:43 -0600

    create the rules file

--- app/gilded_rose.rb
+++ app/gilded_rose.rb
@@ -1,4 +1,17 @@
-class GildedRose
+Reglas
+1. La calidad de un articulo nunca es superior a 50 -> Regla tipo propiedad
+2. La calidad de un articulo nunca es negativa -> Regla tipo propiedad (Ver sulfuro)
+
+
+3. Backstage passes aumenta a 2 cuando Sellin <= 10 or aumentan en 3 si Sellin <=5 if sellin < 1 entonces 0
+4. Aged Brie aumenta su calidad en cada iteracion  en no mas de 50 -> Regla tipo propiedad
+
+5. Sulfuras nunca disminuye su calidad tampoco puede venderse -> Regla tipo propiedad, y EXCEPCIONAL
+
+6. Elemento conjurado se degrada 2 veces mas rapido -> Regla Simple
+
+
+7. Degradación de 1 en 1 en cada iteracion -> Regla Simpleclass GildedRose
 
   def initialize(items)
     @items = items
@@ -6,14 +19,20 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+      
+      
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+        #metodo 1
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1
           end
         end
+        #fin_metodo_1
       else
+
         if item.quality < 50
+          #metodo 2
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
@@ -21,6 +40,7 @@ class GildedRose
                 item.quality = item.quality + 1
               end
             end
+            
             if item.sell_in < 6
               if item.quality < 50
                 item.quality = item.quality + 1
@@ -29,9 +49,13 @@ class GildedRose
           end
         end
       end
+
+
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
       end
+
+      
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
@@ -65,4 +89,8 @@ class Item
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
   end
+end
+
+@items.each do |item|
+  Rule.
 end
--- app/rules.rb
+++ app/rules.rb
@@ -1,2 +1,62 @@
 class Rules
+    def initialize(item)
+        run_rules(item)
+    end
+
+    private 
+    #general class
+    def run_rules(item)
+        type = get_type(item)
+        apply_rules(item, type)
+    end
+
+    #return item type
+    def get_type(item)
+        special_products = ['Backstage', 'Aged Brie', 'Sulfuras', 'Conjured']
+        special_products.select {|product| product == item}
+    end
+
+    def apply_rules(item, type)
+        case type
+        when 'Backstage'
+            backstage_quality_calculator
+        when 'Aged Brie'
+            aged_brie_quality_calculator
+        when 'Sulfuras'
+            sulfuras_quality_calculator
+        when 'Conjured'
+            conjured_quality_calculator
+        else
+            "do default action"
+        end
+    end
+
+    #calculate max quality 50 
+    # if sulfura == 80
+    def check_quality_max(item, type)
+        item.quality = 50 if item.quality > 50
+        item.quality = 80 if type == 'Sulfuras'
+    end
+
+    # calculate min quality (always 0 not negative numbers)
+    
+    def check_quality_min(item)
+        
+    end
+
+    #
+    def backstage_quality_calculator
+    end
+
+    def aged_brie_quality_calculator
+    end
+
+    def sulfuras_quality_calculator
+    end
+
+    def conjured_quality_calculator
+    end
+
+    def normal_quality_calculator
+    end
 end
